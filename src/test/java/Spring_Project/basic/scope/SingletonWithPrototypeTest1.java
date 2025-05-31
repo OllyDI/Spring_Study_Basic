92e4fcdfa483b27336f320ce8ff5464b1d510a84
OllyDI <choiseungho11@hanmail.net>
2025-05-31 14:30:20 +0900

    25.05.31

---
+++ src/test/java/Spring_Project/basic/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,107 @@
+package Spring_Project.basic.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+
+    // 이 방식은 count 가 계속 증가됨 -> 프로토타입의 의미가 사라짐
+//    @Scope("singleton")
+//    @RequiredArgsConstructor
+//    static class ClientBean {
+//        private final PrototypeBean prototypeBean;
+//
+//        public int logic() {
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
+//    }
+
+    // 이러한 Provider 를 통해 프로토타입과 싱글톤 스코프를 사용할 때 매번 새로운 빈 생성 -> 스프링 의존
+//    @Scope("singleton")
+//    @RequiredArgsConstructor
+//    static class ClientBean {
+//
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//
+//        public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
+//    }
+
+
+    // javax 의 Provider -> 자바표준임, 스프링에 의존하지 않음, DL기능만 제공
+    @Scope("singleton")
+    @RequiredArgsConstructor
+    static class ClientBean {
+
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+
+    @Getter
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
